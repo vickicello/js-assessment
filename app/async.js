8c4804a353f340f1d0e9cd26d4bf0745d316076e
vickicello <vicki.cello@gmail.com>
2019-01-21 19:47:39 -0500

    complete async js

--- app/async.js
+++ app/async.js
@@ -1,11 +1,24 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
+  //understand how to use promises to handle asynchronicity
   async: function(value) {
-
+    let def = $.Deferred();
+    setTimeout (function(){
+      def.resolve(value);
+    }, 10);
+    return def.promise();
   },
 
+  //retrieve data from the server and return a sorted array of names
   manipulateRemoteData: function(url) {
-
+    let def = $.Deferred();
+      $.ajax(url).then(function(response){
+        let people = $.map(response.people, function(person){
+          return person.name;
+        });
+        def.resolve(people.sort());
+      });
+      return def.promise();
   }
 };
